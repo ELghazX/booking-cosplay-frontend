8c72fc50e4c8b24c108f95d42d382abdad697b99
ELghazX <2309106041@student.unmul.ac.id>
2025-05-20 17:37:42 +0800

    feat: nambah react route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route } from "react-router";
+import Homepage from './pages/Homepage';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-red-500'>Hello Vite + React!</h1>
-    </>
+    <Routes>
+      {/* ini contoh */}
+      <Route path="/halaman" element={<Homepage />} />
+      <Route path="/login" element={<Homepage />} />
+      <Route path="/register" element={<Homepage />} />
+      <Route path="/detail-produk" element={<Homepage />} />
+      <Route path="/profil/:id" element={<Homepage />} />
+    </Routes>
+
   )
 }
 
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router"
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Kata kata kek</h1>
+
+      <Link to="/">Kembali ke halaman utama</Link>
+
+    </div>
+  )
+}
+
+export default Homepage
